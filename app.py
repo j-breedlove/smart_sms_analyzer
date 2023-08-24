cb15d641e3f1db2f7c8de284ed9d3ebb1fb1b576
j-breedlove <jbreedlove@jasonbreedlove.dev>
2023-08-24 15:26:25 -0700

    add database tables

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+csrf = CSRFProtect(app)
+CORS(app)
+
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sms.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+# CONFIGURE TABLES
+class User(UserMixin, db.Model):
+    # __bind_key__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(250))
+    name = db.Column(db.String(1000))
+    # Add the relationship to Messages
+    messages = relationship("SMS", backref='user', lazy='dynamic')
+
+
+class SMS(db.Model):
+    # __bind_key__ = 'blog'
+    __tablename__ = "sms"
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    direction = db.Column(db.String(10), unique=True, nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    time = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
+
+@app.route('/api/test', methods=['GET'])
+def test():
+    return jsonify(message="Flask and React are connected!"), 200
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Making a GET request to the Flask backend
+    axios.get('http://localhost:5000/api/test')
+        .then((response) => {
+          setData(response.data.message);
+        })
+        .catch((error) => {
+          console.error("Error fetching the data", error);
+        });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        {data ? data : "Loading..."}
+      </div>
   );
 }
 
